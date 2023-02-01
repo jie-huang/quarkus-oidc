362f52bda1d1304f849883af0278879f079e6dcd
jie.huang <jie.huang@tusimple.ai>
2023-02-01 15:52:56 -0800

    create to demo oidc problem

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # quarkus-oidc
+
+This is to test login.
+1. config application.properties to set the oidc parameters
+2. select a version in gradle.properties
+3. access the path /test/login
+
+Result
+- 2.11.1.Final, works well
+- 2.13.7.Final, /oidc/login-callback path cannot find, 404
+- 2.16.1.Final, redirect between /test/login and /oidc/login-callback,   
+```Firefox has detected that the server is redirecting the request for this address in a way that will never complete.```
---
+++ src/main/java/jie/Session.java
@@ -0,0 +1,32 @@
+package jie;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import io.quarkus.oidc.OidcSession;
+
+@Path("/test")
+public class Session {
+  @Inject
+  OidcSession oidcSession;
+
+  @GET
+  @Path("/login")
+  @RolesAllowed("**")
+  public Response login() throws URISyntaxException {
+    return Response.seeOther(new URI("/test/data")).build();
+  }
+
+  @GET
+  @Path("/data")
+  @RolesAllowed("**")
+  public Response data() {
+    return Response.ok("<!doctype html><html><head><title>test</title></head><body>login successfully</body></html>")
+      .header("Content-Type", "text/html;charset=UTF-8").build();
+  }
+}
+
